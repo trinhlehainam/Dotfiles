d796a2d9a90c9a8ee5e8ac836de5f796479f0811
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2024-05-16 18:01:47 +0900

    Add update neovim config shell script to auto create chezmoi file from chezmoitemplates

--- .chezmoitemplates/scripts/update_nvim_config.sh
+++ .chezmoitemplates/scripts/update_nvim_config.sh
@@ -2,19 +2,10 @@
 
 # Determine the operating system
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux
   nvim_config_dir="$HOME/.config/nvim"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS
   nvim_config_dir="$HOME/.config/nvim"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-  # Cygwin (POSIX compatibility layer and Linux environment emulation for Windows)
-  nvim_config_dir="$HOME/AppData/Local/nvim"
-elif [[ "$OSTYPE" == "msys" ]]; then
-  # Git Bash (Windows)
-  nvim_config_dir="$HOME/AppData/Local/nvim"
-elif [[ "$OSTYPE" == "win32" ]]; then
-  # Windows (rare, might need further checks)
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
   nvim_config_dir="$HOME/AppData/Local/nvim"
 else
   echo "Unsupported OS: $OSTYPE"
@@ -31,15 +22,18 @@ mkdir -p "$templates_dir"
 # Copy the Neovim config to .chezmoitemplates
 cp -r "$nvim_config_dir"/* "$templates_dir/"
 
-# Convert files to templates
-find "$templates_dir" -type f -exec sh -c 'mv "$0" "${0}.tmpl"' {} \;
-
 # Create the chezmoi managed files to use the templates
 find "$templates_dir" -type f -exec sh -c '
-  template_file="${0#$chezmoi_config_dir/.chezmoitemplates/}"
-  target_file="$chezmoi_config_dir/dot_${template_file%.tmpl}"
+  chezmoi_config_dir="$1"
+  template_file="${2#$chezmoi_config_dir/.chezmoitemplates/}"
+  target_file="$chezmoi_config_dir/dot_$template_file.tmpl"
+  target_dir="$(dirname "$target_file")"
+  mkdir -p "$target_dir"
+  if [ ! -f "$target_file" ]; then
+    touch "$target_file"
+  fi
   echo "{{ template \"$template_file\" . }}" > "$target_file"
-' {} \;
+' sh "$chezmoi_config_dir" {} \;
 
 # Apply chezmoi configuration
 chezmoi apply
