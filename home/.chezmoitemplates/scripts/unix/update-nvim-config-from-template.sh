99803961eea2a36d7d483d0113153ec84e3c363c
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2024-05-20 01:11:58 +0900

    Change shell script to also detect added and deleted nvim config files

--- home/.chezmoitemplates/scripts/unix/update-nvim-config-from-template.sh
+++ home/.chezmoitemplates/scripts/unix/update-nvim-config-from-template.sh
@@ -3,8 +3,9 @@
 # Define chezmoi directories
 chezmoi_root_dir="$HOME/.local/share/chezmoi/home"
 templates_dir="$chezmoi_root_dir/.chezmoitemplates/nvim"
+state_file="$templates_dir/state.json"
 
-create_template() {
+new_template() {
   local chezmoi_root_dir="$1"
   template_file="${2#$chezmoi_root_dir/.chezmoitemplates/}"
   if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
@@ -25,8 +26,58 @@ create_template() {
   echo "$template_string" > "$target_file"
 }
 
-export -f create_template
-# Create the chezmoi managed files to use the templates
-find "$templates_dir" -type f -exec sh -c '
-  create_template "$1" "$2"
-' sh "$chezmoi_root_dir" {} \;
+# Function to remove a template
+remove_template() {
+    local chezmoi_root_dir=$1
+    local template_file=$2
+    
+    if [ "$OSTYPE" == "msys" ]; then
+        target_file="$chezmoi_root_dir/AppData/Local/$template_file.tmpl"
+    else
+        target_file="$chezmoi_root_dir/dot_config/dot_$template_file.tmpl"
+    fi
+    
+    if [ -f "$target_file" ]; then
+        rm -f "$target_file"
+    fi
+}
+
+# Load previous state if it exists
+declare -A previous_state
+if [ -f "$state_file" ]; then
+    json=$(cat "$state_file")
+    while IFS="=" read -r key value; do
+        previous_state[$key]="$value"
+    done < <(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' <<< "$json")
+fi
+
+# Get current state
+declare -A current_state
+while IFS= read -r -d '' file; do
+    template_file=${file#$chezmoi_root_dir/.chezmoitemplates/}
+    # Ignore create template for state.json file
+    if [[ "$template_file" == *"state.json"* ]]; then
+        continue
+    fi
+    current_state["$template_file"]=$(stat -c %Y "$file")
+done < <(find "$templates_dir" -type f -print0)
+
+# Detect added files
+for file in "${!current_state[@]}"; do
+    if [ ! "${previous_state[$file]+exists}" ]; then
+        echo "Creating template for: $file"
+        new_template "$chezmoi_root_dir" "$file"
+    fi
+done
+
+# Detect deleted files
+for file in "${!previous_state[@]}"; do
+    if [ ! "${current_state[$file]+exists}" ]; then
+        echo "Removing template for: $file"
+        remove_template "$chezmoi_root_dir" "$file"
+    fi
+done
+
+# Save current state
+# TODO: need to install jq first
+declare -p current_state | jq -n 'reduce (input|to_entries[]) as {$key, $value} ({}; .[$key] = $value)' > "$state_file"
