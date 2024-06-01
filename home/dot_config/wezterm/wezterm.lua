f496d6a3c0cf7c29f8daee0b99312205b10b3614
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2024-06-02 04:51:06 +0900

    Add wezterm config.

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Ref: https://github.com/KevinSilvester/wezterm-config
+
+-- require('utils.backdrops'):set_files():random()
+
+require('events.right-status').setup()
+require('events.left-status').setup()
+require('events.tab-title').setup()
+require('events.new-tab-button').setup()
+
+local config = require('config')
+   :init()
+   :append(require('config.appearance'))
+   :append(require('config.bindings'))
+   :append(require('config.domains'))
+   :append(require('config.fonts'))
+   :append(require('config.general'))
+   :append(require('config.launch'))
+
+config.window_background_opacity = 0.8
+
+return config
