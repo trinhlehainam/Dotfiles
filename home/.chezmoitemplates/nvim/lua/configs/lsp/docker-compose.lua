d56ee6c944cf3e81fb53cfe6a0950755a8a995f1
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2024-06-02 18:32:47 +0900

    Update configs to work with docker.

---
+++ home/.chezmoitemplates/nvim/lua/configs/lsp/docker-compose.lua
@@ -0,0 +1,20 @@
+local LanguageSetting = require("configs.lsp.base")
+local M = LanguageSetting:new()
+
+M.lspconfig.server = "docker_compose_language_service"
+M.lspconfig.use_setup = true
+
+M.config = function()
+	-- NOTE: need to set filetype for docker-compose can detect
+	-- https://github.com/neovim/neovim/discussions/26571
+	-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#docker_compose_language_service
+	vim.filetype.add({
+		filename = {
+			["docker-compose.yml"] = "yaml.docker-compose",
+			["docker-compose.yaml"] = "yaml.docker-compose",
+			["compose.yml"] = "yaml.docker-compose",
+			["compose.yaml"] = "yaml.docker-compose",
+		},
+	})
+end
+return M
--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -2,20 +2,22 @@
 
 -- require('utils.backdrops'):set_files():random()
 
-require('events.right-status').setup()
-require('events.left-status').setup()
-require('events.tab-title').setup()
-require('events.new-tab-button').setup()
+require("events.right-status").setup()
+require("events.left-status").setup()
+require("events.tab-title").setup()
+require("events.new-tab-button").setup()
 
-local config = require('config')
-   :init()
-   :append(require('config.appearance'))
-   :append(require('config.bindings'))
-   :append(require('config.domains'))
-   :append(require('config.fonts'))
-   :append(require('config.general'))
-   :append(require('config.launch'))
+local config = require("config")
+	:init()
+	:append(require("config.appearance"))
+	:append(require("config.bindings"))
+	:append(require("config.domains"))
+	:append(require("config.fonts"))
+	:append(require("config.general"))
+	:append(require("config.launch"))
 
+-- NOTE: window_background_opacity does not work as table key
+-- so we need to set it separately
 config.window_background_opacity = 0.8
 
 return config
