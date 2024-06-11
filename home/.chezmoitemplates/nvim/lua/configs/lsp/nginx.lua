23dcf6e91f813819ca18d45e4c310e227c4c9e4e
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2024-06-11 12:09:48 +0900

    add Mason install nginx-langauge-server and find execute to nginx lsp setup

--- home/.chezmoitemplates/nvim/lua/configs/lsp/nginx.lua
+++ home/.chezmoitemplates/nvim/lua/configs/lsp/nginx.lua
@@ -6,21 +6,34 @@ local M = LanguageSetting:new()
 
 M.lspconfig.use_masonlsp_setup = false
 
+local common = require("utils.common")
+
+---@param nginx_lsp_pkg_path string
+local function nginx_lsp_cmd(nginx_lsp_pkg_path)
+	if common.IS_WINDOWS then
+		return nginx_lsp_pkg_path .. "/venv/bin/python3.9.exe"
+	else
+		return nginx_lsp_pkg_path .. ""
+	end
+end
+
 M.after_masonlsp_setup = function()
-	-- local mason_utils = require("utils.mason")
+	local mason_utils = require("utils.mason")
 	local log = require("utils.log")
 
-	-- if not mason_utils.has_mason() then
-	-- 	log.error("Cannot load mason installer")
-	-- 	return
-	-- end
+	if not mason_utils.has_mason() then
+		log.error("Cannot load mason installer")
+		return
+	end
 
 	-- NOTE: mason only install python3.9 execution, not actual nginx_language_server
 	-- need to manually install with pip
-	-- mason_utils.install({ "nginx-language-server" })
+	mason_utils.install({ "nginx-language-server" })
 
-	if vim.fn.executable("nginx-language-server") == 0 then
-		log.error("nginx-language-server is not installed")
+	local nginx_lsp_path = mason_utils.get_mason_package_path("nginx-language-server")
+
+	if not nginx_lsp_path then
+		log.error("nginx-language-server is not installed in mason package")
 		return
 	end
 
@@ -28,6 +41,7 @@ M.after_masonlsp_setup = function()
 	local capabilities = require("utils.lsp").get_cmp_capabilities()
 	-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#nginx_language_server
 	require("lspconfig").nginx_language_server.setup({
+		cmd = { nginx_lsp_cmd(nginx_lsp_path) },
 		capabilities = capabilities,
 		on_attach = on_attach,
 	})
