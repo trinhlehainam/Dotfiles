eb3a9eb99211159c17304f34c45fdc8b6fe92648
WSL Ubuntu Nam Vice <trinhlehainamvice@gmail.com>
2024-06-27 03:17:12 +0900

    Add tmux config and add addition filetypes for detect chezmoi file in Neovim config.

--- home/.chezmoitemplates/nvim/lua/plugins/chezmoi.lua
+++ home/.chezmoitemplates/nvim/lua/plugins/chezmoi.lua
@@ -7,6 +7,19 @@ return {
 	config = function()
 		require("chezmoi").setup({
 			-- your configurations
+			vim.filetype.add({
+				filename = {
+					["dot_env"] = "env",
+					["dot_gitignore"] = "gitignore",
+					["dot_bashrc"] = "sh",
+					["dot_bash_profile"] = "sh",
+					["dot_bash_logout"] = "sh",
+					["dot_bash_history"] = "sh",
+					["dot_bash_aliases"] = "sh",
+					["dot_zshrc"] = "sh",
+					["dot_tmux.conf"] = "tmux",
+				},
+			}),
 		})
 	end,
 }
