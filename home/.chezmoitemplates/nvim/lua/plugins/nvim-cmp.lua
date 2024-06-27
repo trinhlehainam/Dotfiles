063dcd5bad026c6ac0df4f7a0943b8b0f419e468
WSL Ubuntu Nam Vice <trinhlehainamvice@gmail.com>
2024-06-27 18:35:32 +0900

    Move nvim-cmp config when plugin is loaded rather put in after plugin Neovim config.

--- home/.chezmoitemplates/nvim/lua/plugins/nvim-cmp.lua
+++ home/.chezmoitemplates/nvim/lua/plugins/nvim-cmp.lua
@@ -36,6 +36,115 @@ return { -- Autocompletion
 		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-buffer",
-		-- "hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-cmdline",
 	},
+	config = function()
+		local cmp = require("cmp")
+
+		local luasnip = require("luasnip")
+		luasnip.config.setup()
+
+		local lspkind = require("lspkind")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			-- completion = { completeopt = "menu,menuone,noinsert" },
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete({}),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				-- Use [C-p] and [C-n] for navigate backward and forward
+				-- ['<Tab>'] = cmp.mapping(function(fallback)
+				--   if cmp.visible() then
+				--     cmp.select_next_item()
+				--   elseif luasnip.expand_or_jumpable() then
+				--     luasnip.expand_or_jump()
+				--   else
+				--     fallback()
+				--   end
+				-- end, { 'i', 's' }),
+				-- ['<S-Tab>'] = cmp.mapping(function(fallback)
+				--   if cmp.visible() then
+				--     cmp.select_prev_item()
+				--   elseif luasnip.jumpable(-1) then
+				--     luasnip.jump(-1)
+				--   else
+				--     fallback()
+				--   end
+				-- end, { 'i', 's' }),
+
+				-- Think of <c-l> as moving to the right of your snippet expansion.
+				--  So if you have a snippet that's like:
+				--  function $name($args)
+				--    $body
+				--  end
+				--
+				-- <c-l> will move you to the right of each of the expansion locations.
+				-- <c-h> is similar, except moving you backwards.
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { "i", "s" }),
+				["<C-h>"] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			}, {
+				{ name = "crate" },
+			}),
+			formatting = {
+				format = lspkind.cmp_format({
+					before = function(entry, vim_item)
+						local hastailwindtools, tailwindtools = pcall(require, "tailwind-tools.cmp")
+
+						if not hastailwindtools then
+							return vim_item
+						end
+
+						return tailwindtools.lspkind_format(entry, vim_item)
+					end,
+				}),
+			},
+		})
+
+		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+			matching = { disallow_symbol_nonprefix_matching = false },
+		})
+	end,
 }
