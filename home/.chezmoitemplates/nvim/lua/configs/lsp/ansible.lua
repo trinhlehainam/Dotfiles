232b4960e267dda0af940874985b2a57bd84ff44
NamVice <trinhlehainam@protonmail.com>
2025-02-04 04:44:18 +0900

    Add yaml.ansbile regrex file detection in Neovim ansbile config.

--- home/.chezmoitemplates/nvim/lua/configs/lsp/ansible.lua
+++ home/.chezmoitemplates/nvim/lua/configs/lsp/ansible.lua
@@ -6,6 +6,17 @@ local common = require("utils.common")
 
 M.treesitter.filetypes = { "yaml" }
 
+-- INFO: https://github.com/ansible/vscode-ansible?tab=readme-ov-file#without-file-inspection
+-- INFO: https://docs.ansible.com/ansible/latest/tips_tricks/sample_setup.html#sample-directory-layout
+vim.filetype.add({
+	pattern = {
+		[".*/playbooks/.*%.ya?ml"] = "yaml.ansible", -- yaml files under /playbooks/ directory
+		[".*%.ansible%.ya?ml"] = "yaml.ansible", -- files with the following double extension: .ansible.yml or .ansible.yaml.
+		["site%.ya?ml"] = "yaml.ansible", -- notable yaml names recognized by ansible like site.yml or site.yaml
+		[".*playbook.*%.ya?ml"] = "yaml.ansible", -- yaml files having playbook in their filename: *playbook*.yml or *playbook*.yaml
+	},
+})
+
 -- INFO: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ansiblels
 local ansiblels = LspConfig:new("ansiblels")
 ansiblels.server = "ansiblels"
