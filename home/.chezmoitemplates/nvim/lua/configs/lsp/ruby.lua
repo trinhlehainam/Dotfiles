d1a7c4853572c11126784756ad1dd37f135946eb
NamVice <trinhlehainam@protonmail.com>
2025-02-16 21:20:45 +0900

    Update requirement log to install ruby-lsp.

--- home/.chezmoitemplates/nvim/lua/configs/lsp/ruby.lua
+++ home/.chezmoitemplates/nvim/lua/configs/lsp/ruby.lua
@@ -1,6 +1,17 @@
 local LanguageSetting = require('configs.lsp.base')
 local LspConfig = require('configs.lsp.lspconfig')
 local M = LanguageSetting:new()
+
+local log = require('utils.log')
+
+if not vim.fn.executable('ruby') then
+  log.info('ruby-lsp requires ruby to be installed')
+  log.info(
+    'install ruby following instructions: https://github.com/rbenv/rbenv?tab=readme-ov-file#using-package-managers'
+  )
+  return M
+end
+
 M.treesitter.filetypes = { 'ruby' }
 
 M.formatterconfig.servers = { 'rubocop' }
@@ -14,7 +25,6 @@ M.linterconfig.linters_by_ft = {
 }
 
 -- Use rbenv to manage Ruby versions: https://github.com/rbenv/rbenv?tab=readme-ov-file#using-package-managers
--- Install ruby-lsp with mason: https://shopify.github.io/ruby-lsp/editors.html#mason
 -- To solve cannot load such file -- zlib on WSL: https://stackoverflow.com/a/78110911
 M.lspconfigs = { LspConfig:new('ruby_lsp') }
 
