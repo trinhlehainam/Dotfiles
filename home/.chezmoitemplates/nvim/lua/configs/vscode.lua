feb1a2237f8dac9941e2a7c7287f713de65dad4b
Trinh Le Hai Nam <64761941+trinhlehainam@users.noreply.github.com>
2025-06-28 14:51:35 +0900

    refactor(keybindings): reorganize Neovim and IdeaVim keymaps (#4)
    Refactor keybindings in Neovim and IdeaVim configurations for improved consistency and organization.
    
    - LSP-related keybindings now use a `gr` prefix (e.g., `grn` for rename, `gra` for quickfix).
    - Search/file-related keybindings now use an `s` prefix (e.g., `sf` for quick open, `sg` for find in files).
    - Added a new "Git" section to `home/dot_ideavimrc` with relevant keymaps.

--- home/.chezmoitemplates/nvim/lua/configs/vscode.lua
+++ home/.chezmoitemplates/nvim/lua/configs/vscode.lua
@@ -23,28 +23,38 @@ vim.keymap.set('n', 'bj', ':Tabprevious<CR>', opts.nore)
 vim.keymap.set('n', 'bk', ':Tabnext<CR>', opts.nore)
 vim.keymap.set('n', 'bc', ':Tabclose<CR>', opts.nore)
 
-vim.keymap.set('n', 'tt', "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>", opts.nore)
+vim.keymap.set(
+  'n',
+  '<leader>tt',
+  "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>",
+  opts.nore
+)
 vim.keymap.set('n', '<leader>/', "<Cmd>call VSCodeNotify('actions.find')<CR>", opts.nore)
 
 -- Telescope
 vim.keymap.set(
   'n',
-  '<leader>ff',
+  '<leader>sf',
   "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>",
   opts.nore
 )
 vim.keymap.set(
   'n',
-  '<leader>fg',
+  '<leader>sg',
   "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>",
   opts.nore
 )
 
 -- LSP
-vim.keymap.set('n', '<leader>rn', "<Cmd>call VSCodeNotify('editor.action.rename')<CR>", opts.nore)
-vim.keymap.set('n', '<leader>ca', "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>", opts.nore)
-vim.keymap.set('n', 'gd', "<Cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>", opts.nore)
-vim.keymap.set('n', 'gr', "<Cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", opts.nore)
+vim.keymap.set('n', 'grn', "<Cmd>call VSCodeNotify('editor.action.rename')<CR>", opts.nore)
+vim.keymap.set('n', 'gra', "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>", opts.nore)
+vim.keymap.set(
+  'n',
+  'grd',
+  "<Cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>",
+  opts.nore
+)
+vim.keymap.set('n', 'grr', "<Cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", opts.nore)
 vim.keymap.set(
   'n',
   '[d',
