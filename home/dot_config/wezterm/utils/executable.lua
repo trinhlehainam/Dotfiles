66859e1814215b05deff33606231fa249ea824c0
Trinh Le Hai Nam <trinhlehainam@protonmail.com>
2025-12-17 00:16:19 +0900

    docs(wezterm): document executable detection helper

--- home/dot_config/wezterm/utils/executable.lua
+++ home/dot_config/wezterm/utils/executable.lua
@@ -1,3 +1,19 @@
+---Utility helpers to safely detect executables in PATH.
+---
+---Why this exists:
+--- - Some configs want to dynamically build `launch_menu` entries (or keybindings)
+---   based on which shells/tools are actually installed.
+--- - Avoids `os.execute(...)` checks on Windows which can spawn visible shell
+---   windows and slow down startup when used repeatedly.
+---
+---Implementation notes:
+--- - Uses `wezterm.run_child_process` so checks are silent/non-interactive.
+--- - Caches results per executable name to avoid repeating process spawns.
+---
+---References:
+--- - https://github.com/pasanec/wezterm_win/blob/main/wezterm.lua
+--- - https://github.com/wezterm/wezterm/issues/5963#issuecomment-2533250740
+---
 local wezterm = require('wezterm') ---@type Wezterm
 
 local platform = require('utils.platform')
@@ -7,6 +23,12 @@ local M = {}
 ---@type table<string, boolean>
 local cache = {}
 
+---Checks whether `executable` can be resolved from `PATH`.
+---
+---On Windows, uses `where.exe /Q`.
+---On non-Windows, uses `sh -c "command -v ..."`.
+---
+---Result is cached for the lifetime of the config Lua state.
 ---@param executable string
 ---@return boolean
 function M.exists(executable)
