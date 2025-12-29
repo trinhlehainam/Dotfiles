122dbab80b15425e466bd460de9f38ef71a9c921
Trinh Le Hai Nam <trinhlehainam@protonmail.com>
2025-12-29 18:52:29 +0900

    refactor(nvim): switch default colorscheme from gruvbox-material to kanagawa

--- home/.chezmoitemplates/nvim/lua/plugins/colorschemes.lua
+++ home/.chezmoitemplates/nvim/lua/plugins/colorschemes.lua
@@ -1,19 +1,21 @@
 return {
-  'rebelot/kanagawa.nvim',
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme('kanagawa')
+    end,
+  },
   'folke/tokyonight.nvim',
   'navarasu/onedark.nvim',
   {
     'f4z3r/gruvbox-material.nvim',
     name = 'gruvbox-material',
-    lazy = false,
-    priority = 1000,
     -- https://github.com/f4z3r/gruvbox-material.nvim?tab=readme-ov-file#usage-and-configuration
     opts = {
       constrast = 'hard',
     },
-    config = function()
-      vim.cmd.colorscheme('gruvbox-material')
-    end,
   },
   {
     'catppuccin/nvim',
