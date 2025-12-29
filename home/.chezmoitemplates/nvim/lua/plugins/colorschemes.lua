cdd6baf3ca1e1981f7f8f48a32ee36523907a3d0
Trinh Le Hai Nam <trinhlehainam@protonmail.com>
2025-12-29 18:32:39 +0900

    refactor(nvim): switch default colorscheme to gruvbox-material with hard contrast

--- home/.chezmoitemplates/nvim/lua/plugins/colorschemes.lua
+++ home/.chezmoitemplates/nvim/lua/plugins/colorschemes.lua
@@ -2,14 +2,21 @@ return {
   'rebelot/kanagawa.nvim',
   'folke/tokyonight.nvim',
   'navarasu/onedark.nvim',
-  'sainnhe/gruvbox-material',
-  'ellisonleao/gruvbox.nvim',
-  'sainnhe/everforest',
+  {
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    -- https://github.com/f4z3r/gruvbox-material.nvim?tab=readme-ov-file#usage-and-configuration
+    opts = {
+      constrast = 'hard',
+    },
+    config = function()
+      vim.cmd.colorscheme('gruvbox-material')
+    end,
+  },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
-    config = function()
-      vim.cmd.colorscheme('catppuccin')
-    end,
   },
 }
