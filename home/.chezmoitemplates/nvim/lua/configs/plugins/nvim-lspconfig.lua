286d9e253434cf1031446ceec64d88724a98ec1c
Trinh Le Hai Nam <trinhlehainam@protonmail.com>
2026-01-02 04:49:28 +0900

    refactor(lsp): simplify LSP configuration comments

--- home/.chezmoitemplates/nvim/lua/configs/plugins/nvim-lspconfig.lua
+++ home/.chezmoitemplates/nvim/lua/configs/plugins/nvim-lspconfig.lua
@@ -1,82 +1,36 @@
--- Brief aside: **What is LSP?**
---
--- LSP is an initialism you've probably heard, but might not understand what it is.
---
--- LSP stands for Language Server Protocol. It's a protocol that helps editors
--- and language tooling communicate in a standardized fashion.
---
--- In general, you have a "server" which is some tool built to understand a particular
--- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
--- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
--- processes that communicate with some "client" - in this case, Neovim!
---
--- LSP provides Neovim with features like:
---  - Go to definition
---  - Find references
---  - Autocompletion
---  - Symbol Search
---  - and more!
---
--- Thus, Language Servers are external tools that must be installed separately from
--- Neovim. This is where `mason` and related plugins come into play.
---
--- If you're wondering about lsp vs treesitter, you can check out the wonderfully
--- and elegantly composed help section, `:help lsp-vs-treesitter`
-
---  This function gets run when an LSP attaches to a particular buffer.
---    That is to say, every time a new file is opened that is associated with
---    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
---    function will be executed to configure the current buffer
+-- LSP buffer-local setup (keymaps, highlights, inlay hints)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
   callback = function(event)
-    -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-    -- to define small helper and utility functions so you don't have to repeat yourself.
-    --
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    -- Helper for buffer-local LSP keymaps
     local map = function(keys, func, desc, mode)
       mode = mode or 'n'
       vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
-    -- Find references for the word under your cursor.
     map('grr', function()
       Snacks.picker.lsp_references()
     end, '[G]oto [R]eferences')
 
-    -- Jump to the implementation of the word under your cursor.
-    --  Useful when your language has ways of declaring types without an actual implementation.
     map('gri', function()
       Snacks.picker.lsp_implementations()
     end, '[G]oto [I]mplementation')
 
-    -- Jump to the definition of the word under your cursor.
-    --  This is where a variable was first declared, or where a function is defined, etc.
-    --  To jump back, press <C-t>.
     map('grd', function()
       Snacks.picker.lsp_definitions()
     end, '[G]oto [D]efinition')
 
-    -- WARN: This is not Goto Definition, this is Goto Declaration.
-    --  For example, in C this would take you to the header.
+    -- Declaration (not definition)
     map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-    -- Fuzzy find all the symbols in your current document.
-    --  Symbols are things like variables, functions, types, etc.
     map('gO', function()
       Snacks.picker.lsp_symbols()
     end, 'Open Document Symbols')
 
-    -- Fuzzy find all the symbols in your current workspace.
-    --  Similar to document symbols, except searches over your entire project.
     map('gW', function()
       Snacks.picker.lsp_workspace_symbols()
     end, 'Open Workspace Symbols')
 
-    -- Jump to the type of the word under your cursor.
-    --  Useful when you're not sure what type a variable is and you want to see
-    --  the definition of its *type*, not where it was *defined*.
     map('grt', function()
       Snacks.picker.lsp_type_definitions()
     end, '[G]oto [T]ype Definition')
@@ -89,11 +43,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       return client:supports_method(method, bufnr)
     end
 
-    -- The following two autocommands are used to highlight references of the
-    -- word under your cursor when your cursor rests there for a little while.
-    --    See `:help CursorHold` for information about when this is executed
-    --
-    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+    -- Document highlights on CursorHold
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if
       client
@@ -126,10 +76,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       })
     end
 
-    -- The following code creates a keymap to toggle inlay hints in your
-    -- code, if the language server you are using supports them
-    --
-    -- This may be unwanted, since they displace some of your code
+    -- Inlay hints toggle (if supported)
     if
       client
       and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
@@ -141,8 +88,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
--- Diagnostic Config
--- See :help vim.diagnostic.Opts
+-- Diagnostics
 local diagnostic_jump_ns = vim.api.nvim_create_namespace('on_diagnostic_jump')
 vim.diagnostic.config({
   severity_sort = true,
@@ -160,8 +106,7 @@ vim.diagnostic.config({
   virtual_lines = {
     current_line = true,
   },
-  -- NOTE: `vim.diagnostic.config({ jump = { on_jump = ... } })` is only available starting in Neovim 0.12.
-  -- Reference: https://github.com/neovim/neovim/issues/33154
+  -- Neovim 0.12+: jump.on_jump (see https://github.com/neovim/neovim/issues/33154)
   jump = (function()
     if vim.fn.has('nvim-0.12') == 1 then
       return {
@@ -184,7 +129,7 @@ vim.diagnostic.config({
 
 -- Diagnostic keymaps
 
--- Diagnostic handlers can be toggled. Follow `:help diagnostic-toggle-virtual-lines-example`.
+-- Toggle diagnostic virtual_lines
 vim.keymap.set('n', 'gK', function()
   local new_config = not vim.diagnostic.config().virtual_lines
   vim.diagnostic.config({ virtual_lines = new_config })
